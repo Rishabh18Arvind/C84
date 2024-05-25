999ab1e99baae84fe01852b41f89dfce4098bc44
Rishabh18Arvind <153378447+Rishabh18Arvind@users.noreply.github.com>
2024-05-25 11:01:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,81 @@
+// Create a reference for the canvas
+canvas=document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
+
+img_width = 300;
+img_height = 100;
+
+var img_image;
+
+img_x = 100;
+img_y = 100;
+
+function add() {
+	img_imgTag = new Image(); //defining a variable with a new image
+	img_imgTag.onload = uploadimg; // setting a function, onloading this variable
+	img_imgTag.src = img_image;   // load image
+}
+
+function uploadimg() {
+
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
+}
+
+window.addEventListener("keydown",my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	
+		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90)){
+		alphabetkey();
+		document.getElementById("d1").innerHTML="You pressed Alphabet Key";
+		console.log("alphabet key");
+		}
+		else{
+			otherkey();
+			
+		}
+	if((keyPessed =17,18,27)){
+		Specialkey();
+		document.getElementById("d2").innerHTML="You pressed special key";
+		console.log("special key");
+	}
+	if((keyPressed >=48 && keyPressed<=57)){
+		numberkey();
+		document.getElementById("d3");
+        console.log("number key")
+	}
+
+}
+
+function alphabetkey()
+{ 
+	img_imahe="Alpkey.png";
+	add();
+}
+function numberkey()
+{
+	img_imahe="numkey.png";
+	add();
+}
+
+function arrowkey()
+{
+	img_imahe="Arrkey.png";
+	add();
+}
+
+function specialkey()
+{
+	img_imahe="spkey.png";
+	add();
+}
+
+function otherkey()
+{
+	img_image="otherkey.png";
+	add();
+}
+	
